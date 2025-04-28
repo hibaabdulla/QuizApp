f77c8d7e143c16323501c9f4a7661a77429cf5fe
Hiba Abdulla <hibaabdullahkk@gmail.com>
2025-04-27 18:37:22 -0700

    Check Answer Using 2D Array

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -8,22 +8,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var falseButton: UIButton!
-    let quiz = ["Four + Two = Six", "Three x Three = Nine", "Five - One = Four", "Ten - Five = Five", "Seven + Three = Ten"]
+    let quiz = [["Eight -  Two = Six", "True"], ["Three x Three = Nine", "True"], ["Five + One = Four", "False"]]
     var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = quiz[questionNumber]
+        questionLabel.text = quiz[questionNumber][0]
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let answerPressed = sender.currentTitle!
+        let  correctAnswer = quiz[questionNumber][1]
+        
+        if answerPressed == correctAnswer {
+            print("Correct!")
+        } else {
+            print("Incorrect!")
+        }
+        
         changeQuestion()
     }
     
     func changeQuestion() {
         if questionNumber+1 < quiz.count {
             questionNumber = (questionNumber + 1)
-            questionLabel.text = quiz[questionNumber]
+            questionLabel.text = quiz[questionNumber][0]
+        }
+        else {
+            questionNumber = 0
+            questionLabel.text = quiz[questionNumber][0]
         }
     }
 }
