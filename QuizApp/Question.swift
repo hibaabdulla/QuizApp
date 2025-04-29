04c937762f73bbe109251b07b446f1a3ecef18cf
Hiba Abdulla <hibaabdullahkk@gmail.com>
2025-04-29 00:06:05 -0700

    Create Question Struct

--- QuizApp/Question.swift
+++ QuizApp/Question.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -8,18 +8,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var falseButton: UIButton!
-    let quiz = [["Eight -  Two = Six", "True"], ["Three x Three = Nine", "True"], ["Five + One = Four", "False"]]
+    let quiz = [Question(text: "Eight -  Two = Six", answer: "True"),
+                Question(text: "Three x Three = Nine", answer: "True"),
+                Question(text: "Five + One = Four", answer: "False")]
+    
     var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = quiz[questionNumber][0]
+        questionLabel.text = quiz[questionNumber].text
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
         let answerPressed = sender.currentTitle!
-        let  correctAnswer = quiz[questionNumber][1]
+        let  correctAnswer = quiz[questionNumber].answer
         
         if answerPressed == correctAnswer {
             print("Correct!")
@@ -33,11 +36,11 @@ class ViewController: UIViewController {
     func changeQuestion() {
         if questionNumber+1 < quiz.count {
             questionNumber = (questionNumber + 1)
-            questionLabel.text = quiz[questionNumber][0]
+            questionLabel.text = quiz[questionNumber].text
         }
         else {
             questionNumber = 0
-            questionLabel.text = quiz[questionNumber][0]
+            questionLabel.text = quiz[questionNumber].text
         }
     }
 }
